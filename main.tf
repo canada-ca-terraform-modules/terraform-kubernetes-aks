8238c656609f8749821918b06cd7db7ab2e9cf25
William Hearn <sylus1984@gmail.com>
2019-08-05 16:57:26 -0400

    feat(velero): Add velero resources

--- main.tf
+++ main.tf
@@ -18,6 +18,32 @@ resource "azurerm_resource_group" "rg_aks_development" {
   location = "${var.location}"
 }
 
+resource "azurerm_resource_group" "rg_velero_development" {
+  name     = "${var.prefix}-velero-development"
+  location = "${var.location}"
+}
+
+# Storage Accounts
+
+resource "azurerm_storage_account" "sa_velero_development" {
+  name                = "${var.prefix}velero${random_string.velero_storage_account.result}"
+  location            = "${azurerm_resource_group.rg_velero_development.location}"
+  resource_group_name = "${azurerm_resource_group.rg_velero_development.name}"
+  account_tier = "Standard"
+  account_kind = "BlobStorage"
+  account_replication_type = "GRS"
+  # enable_blob_encryption = "True"
+  # enable_file_encryption = "True"
+  access_tier = "Hot"
+}
+
+resource "azurerm_storage_container" "sc_velero_development" {
+  name                  = "velero"
+  resource_group_name = "${azurerm_resource_group.rg_velero_development.name}"
+  storage_account_name  = "${azurerm_storage_account.sa_velero_development.name}"
+  container_access_type = "private"
+}
+
 # Virtual Networks
 
 resource "azurerm_virtual_network" "vnet_aks_development" {
@@ -133,6 +159,8 @@ resource "azuread_service_principal_password" "server" {
   }
 }
 
+# Passwords
+
 resource "random_string" "application_server_password" {
   length  = 16
   special = true
@@ -140,6 +168,18 @@ resource "random_string" "application_server_password" {
   keepers = {
     service_principal = "${azuread_service_principal.server.id}"
   }
+}
+
+resource "random_string" "velero_password" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "random_string" "velero_storage_account" {
+  length  = 5
+  special = false
+  upper   = false
 }
 
 # Azure AD Client
@@ -192,6 +232,68 @@ resource "random_string" "application_client_password" {
 
   keepers = {
     service_principal = "${azuread_service_principal.client.id}"
+  }
+}
+
+# Velero
+
+resource "azuread_application" "velero" {
+  name                    = "k8s_velero"
+  reply_urls              = ["http://k8s_velero"]
+  type                    = "webapp/api"
+  group_membership_claims = "All"
+
+  required_resource_access {
+    # Windows Azure Active Directory API
+    resource_app_id = "00000002-0000-0000-c000-000000000000"
+
+    resource_access {
+      # DELEGATED PERMISSIONS: "Sign in and read user profile":
+      id   = "311a71cc-e848-46a1-bdf8-97ff7156d8e6"
+      type = "Scope"
+    }
+  }
+
+  required_resource_access {
+    # MicrosoftGraph API
+    resource_app_id = "00000003-0000-0000-c000-000000000000"
+
+    # APPLICATION PERMISSIONS: "Read directory data":
+    resource_access {
+      id   = "7ab1d382-f21e-4acd-a863-ba3e13f7da61"
+      type = "Role"
+    }
+
+    # DELEGATED PERMISSIONS: "Sign in and read user profile":
+    resource_access {
+      id   = "e1fe6dd8-ba31-4d61-89e7-88639da4683d"
+      type = "Scope"
+    }
+
+    # DELEGATED PERMISSIONS: "Read directory data":
+    resource_access {
+      id   = "06da0dbc-49e2-44d2-8312-53f166ab848a"
+      type = "Scope"
+    }
+  }
+}
+
+resource "azuread_service_principal" "velero" {
+  application_id = "${azuread_application.velero.application_id}"
+}
+
+resource "azuread_service_principal_password" "velero" {
+  service_principal_id = "${azuread_service_principal.velero.id}"
+  value                = "${random_string.velero_password.result}"
+  end_date             = "${timeadd(timestamp(), "87600h")}" # 10 years
+
+  # The end date will change at each run (terraform apply), causing a new password to
+  # be set. So we ignore changes on this field in the resource lifecyle to avoid this
+  # behaviour.
+  # If the desired behaviour is to change the end date, then the resource must be
+  # manually tainted.
+  lifecycle {
+    ignore_changes = ["end_date"]
   }
 }
 
